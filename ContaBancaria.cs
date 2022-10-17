6a2c00e2386e20188360586edf35132b0ac6f48a
jonesmedeiros <jones.robertomedeirosdias@gmail.com>
2022-10-17 10:30:32 -0300

    Inserção de pastas

---
+++ ContaBancaria.cs
@@ -0,0 +1,50 @@
+using System.Globalization;
+
+namespace Banco;
+
+    class ContaBancaria{
+    
+        public int Numero { get ; private set;}
+        public string _titular = string.Empty;
+        public double Saldo{get; private set;}
+              
+        //primeiro Construtor com dois parametros
+        public ContaBancaria(int numero, string titular ){
+                Numero = numero;
+                _titular = titular; 
+
+        }
+        //segundo Construtor com três parametros
+            public ContaBancaria(int numero, string titular, double depositoInicial){ 
+                Numero = numero;
+                _titular = titular; 
+                 Deposito(depositoInicial);
+            }
+
+            /*public string Titular{
+                get{ return _titular;}
+                set{ 
+                    if (value != null && value.Length > 1) {
+                        _titular = value;}
+                 }
+            }*/
+
+            // Imprimindo dados da conta
+            public override string ToString(){
+                return "Conta: "
+                + Numero
+                + ", Titular: "
+                +_titular
+                +", Saldo: "
+                +Saldo.ToString("F2", CultureInfo.InvariantCulture);
+
+            }
+            //primeiro metodo
+            public void Deposito(double quantia){
+                Saldo += quantia;
+            }
+             //segundo metodo
+            public void Saque(double quantia){
+                Saldo -= quantia + 5.0;
+            }
+     }
